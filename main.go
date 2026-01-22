b13d7aaf0a5226e1c8a7c947bc1419462497fbcc
 Bagus Rizky 🦊 <mohamad_rizky@bca.co.id>
2026-01-22 21:09:41 +0700

    init

---
+++ main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type Product struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+	Stok  int    `json:"stok"`
+}
+
+var product = []Product{
+	{ID: 1, Name: "Indomie Goreng", Price: 3000, Stok: 10},
+	{ID: 2, Name: "Indomie Ayam Bawang", Price: 3000, Stok: 10},
+	{ID: 3, Name: "Indomie Kari Ayam", Price: 3000, Stok: 10},
+	{ID: 4, Name: "Indomie Ayam Geprek", Price: 3000, Stok: 10},
+	{ID: 5, Name: "Indomie Kari Special", Price: 3000, Stok: 10},
+}
+
+func getProductById(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/v1/products/")
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		http.Error(w, "Invalid product ID", http.StatusBadRequest)
+		return
+	}
+
+	for _, p := range product {
+		if p.ID == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(p)
+			return
+		}
+	}
+	http.Error(w, "Product not found", http.StatusNotFound)
+}
+
+func updateProductById(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/v1/products/")
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		http.Error(w, "Invalid product ID", http.StatusBadRequest)
+		return
+	}
+
+	var updateProduct Product
+	err = json.NewDecoder(r.Body).Decode(&updateProduct)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	for i, p := range product {
+		if p.ID == id {
+			updateProduct.ID = p.ID
+			product[i] = updateProduct
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(updateProduct)
+			return
+		}
+	}
+	http.Error(w, "Product not found", http.StatusNotFound)
+}
+
+func deleteProductById(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/v1/products/")
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "Invalid product ID",
+		})
+		return
+	}
+
+	for i, p := range product {
+		if p.ID == id {
+			product = append(product[:i], product[i+1:]...)
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(map[string]string{
+				"message": "Product deleted successfully",
+			})
+			return
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(w).Encode(map[string]string{
+		"error": "Product not found",
+	})
+}
+
+func main() {
+	// Product Detail
+	http.HandleFunc("/api/v1/products/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			getProductById(w, r)
+		} else if r.Method == "PUT" {
+			updateProductById(w, r)
+		} else if r.Method == "DELETE" {
+			deleteProductById(w, r)
+		}
+	})
+
+	// Products
+	http.HandleFunc("/api/v1/products", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(product)
+		} else if r.Method == "POST" {
+			var newProduct Product
+			err := json.NewDecoder(r.Body).Decode(&newProduct)
+			if err != nil {
+				http.Error(w, "Invalid request", http.StatusBadRequest)
+				return
+			}
+			newProduct.ID = len(product) + 1
+			product = append(product, newProduct)
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+			json.NewEncoder(w).Encode(newProduct)
+		}
+	})
+
+	// Check Server Running
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{
+			"status":  "OK",
+			"message": "Server is running",
+		})
+	})
+
+	fmt.Println("Server running di 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
